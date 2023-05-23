900ae50252e3242380260e6459c9e0389cac1f8d
danielebd <danielebalebalestro@gmail.com>
2023-05-23 21:58:35 +0200

    FK type to Projects

---
+++ database/migrations/2023_05_23_192541_add_type_id_foreign_key_to_projects_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //creazione della FK all'interno della tabella projects
+        Schema::table('projects', function (Blueprint $table) {
+
+            $table->unsignedBigInteger('type_id')->nullable()->after('id');
+
+            $table->foreign('type_id')->references('id')->on('types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+
+            //eliminazione relazione
+            $table->dropForeign(['type_id']);
+
+            //eliminazione colonna
+            $table->dropColumn('type_id');
+
+        });
+    }
+};
